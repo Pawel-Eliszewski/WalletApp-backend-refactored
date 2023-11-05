1a537ad8fdd8cc10d9c29d33d11bdb37b31703f9
Pawel-Eliszewski <eliszewskipawel@gmail.com>
2023-11-05 21:47:09 +0100

    Fixed localhost URL in Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Origin",
     "https://finance-app-wallet.netlify.app"
   );
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
