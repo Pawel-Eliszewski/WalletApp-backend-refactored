ba89a1219b810525993d2c0c1ad24e75ca3b4771
Pawel-Eliszewski <eliszewskipawel@gmail.com>
2023-11-05 17:20:35 +0100

    Fixed firstname in register schema

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,53 +1,53 @@
-const User = require('../schemas/user.schema');
+const User = require("../schemas/user.schema");
 
 const findUserByEmail = async (email) => {
-    return User.findOne({email});
-}
+  return User.findOne({ email });
+};
 const findUserById = async (userId) => {
-    return User.findOne({_id: userId});
-}
-const registerUser = async (email, password) => {
-    return User.create({email, password});
-}
+  return User.findOne({ _id: userId });
+};
+const registerUser = async (email, password, firstname) => {
+  return User.create({ email, password, firstname });
+};
 
 const authenticateUser = async (email, password) => {
-    return User.findOne({email, password});
-}
+  return User.findOne({ email, password });
+};
 
 const setToken = async (email, token) => {
-    return User.updateOne({email}, {token});
-}
+  return User.updateOne({ email }, { token });
+};
 
 const handleUserBalance = async (type, amount, owner) => {
-    const user = await findUserById(owner);
-    if (!user) {
-        return {message: "User not found"};
-    }
-    let balance = user.balance;
-    if (type === 'income') {
-        balance += amount;
-    } else if (type === 'expense') {
-        balance -= amount;
-    }
-    return User.updateOne({_id: owner}, {balance: balance});
+  const user = await findUserById(owner);
+  if (!user) {
+    return { message: "User not found" };
+  }
+  let balance = user.balance;
+  if (type === "income") {
+    balance += amount;
+  } else if (type === "expense") {
+    balance -= amount;
+  }
+  return User.updateOne({ _id: owner }, { balance: balance });
 };
 
 const getUserBalance = async (owner) => {
-    const user = await findUserById(owner);
-    return user.balance;
+  const user = await findUserById(owner);
+  return user.balance;
 };
 
-const updateUserBalance = async(owner, balance) => {
-    return User.updateOne({_id: owner}, {balance: balance});
-}
+const updateUserBalance = async (owner, balance) => {
+  return User.updateOne({ _id: owner }, { balance: balance });
+};
 
 module.exports = {
-    findUserByEmail,
-    registerUser,
-    authenticateUser,
-    setToken,
-    findUserById,
-    handleUserBalance,
-    getUserBalance,
-    updateUserBalance,
-}
+  findUserByEmail,
+  registerUser,
+  authenticateUser,
+  setToken,
+  findUserById,
+  handleUserBalance,
+  getUserBalance,
+  updateUserBalance,
+};
