d6f5b5f159ea21cf4112ba0f96dcf4e78095049d
Pawel-Eliszewski <eliszewskipawel@gmail.com>
2023-11-06 09:58:03 +0100

    Removed Access-Control-Allow-Origin URLs

--- app.js
+++ app.js
@@ -16,16 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://finance-app-wallet.netlify.app"
-  );
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 app.use("/user", usersRouter);
 app.use("/transaction", transactionsRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
